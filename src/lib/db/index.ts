f79f282842670183226c03fb5caf7b30b1222c9c
IB. govt <datsmeayankhan8055@gmail.com>
2025-08-23 17:45:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,8 @@
 
+import "dotenv/config";
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
-import "dotenv/config";
 
 if (!process.env.TURSO_DATABASE_URL) {
   throw new Error("TURSO_DATABASE_URL is not set in the environment variables");
